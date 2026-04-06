2af8183ed1fdd40a3d4c337c67b988563f52ed68
Emanuel Stults <e.d.stults@gmail.com>
2026-04-06 07:59:17 -0400

    Update items.h

--- items.h
+++ items.h
@@ -12,7 +12,7 @@ enum itemNames {NOITEM = 0, DAGGER = 1, SWORD = 2, AXE = 3, ORB = 4, CHALICE = 5
 extern const struct item ITEMS[]; 
 
 int idOf(char*); //put the name of the item in to assign the appropriate id in an inventory array, eg you can say
-		 // idOf("ruby"); to place 6 in the array of ints to be called back later
+		 // idOf("ruby"); to place 6 in the array of ints to be called back later.
 
 
 
